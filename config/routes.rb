ff888fa360b655b8ba619ac5fc3de236b2715b0d
Moataz Elsayed <moataz277@gmail.com>
2015-08-22 22:00:46 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root 'user#home'
   get '/search' => 'posts#search'
+  get 'posts/MyQuestions' => 'posts#OwnQuestions'
   resources :posts , :except => [:index]
   devise_for :users, :controllers => { registrations: 'users/registrations' , sessions: 'users/sessions' }
 end
