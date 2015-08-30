f5164ce275e6478862973bfbf51802b9e113ff80
ahmedKhaled14 <eng.ahmed.khaled@hotmail.com>
2015-08-30 09:38:08 +0200

    vendor compiled assets

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Ezay
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
-   config.serve_static_files = true 
+   config.serve_static_files = true
+   config.serve_static_assets = true 
   end
 end
