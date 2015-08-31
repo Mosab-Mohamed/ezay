86802e85e435a15425dbe32a65f8fe5495110947
mosab muhammad khames <mosab.mohamed94@yahoo.com>
2015-08-31 02:28:11 +0200

    yarb tsht3'l

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-	:address				=> 'smtp',
-	:port					=> '587',
-	:authentication			=> :plain,
-	:user_name				=> 'mosab',
-	:password				=> 'Mo011994292988',
-	:domain					=> 'heroku.com',
-	:enable_starttls_auto	=> true
-}
