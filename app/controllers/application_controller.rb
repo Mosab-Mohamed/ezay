fb71c385dbcc46f8641472d8e801b55d027ace81
Moataz Elsayed <moataz277@gmail.com>
2015-08-31 15:46:19 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def recommended_posts
-	  $rec_posts = Post.where(:category => current_user.department).order("comments_length+likes DESC");
+	  $rec_posts = Post.where(:category => current_user.department).where.not(:user_id => current_user.id).order("comments_length+likes DESC");
   end
 
   helper_method :recommended_posts 
