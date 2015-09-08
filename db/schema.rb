a1ceb03c9b3a2494ca8fbd11ba10154de2ba92a2
mosab muhammad khames <mosab.mohamed94@yahoo.com>
2015-09-08 17:12:22 +0200

    run pending migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831223143) do
+ActiveRecord::Schema.define(version: 20150831204346) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 20150831223143) do
     t.string   "category"
     t.integer  "user_id"
     t.string   "kind"
-    t.integer  "comments_length", default: 0
+    t.integer  "comments_length"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "likes",           default: 0
